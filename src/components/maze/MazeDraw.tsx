e3498dfdb418b02c6c28e3b7d69ed3250b873396
Abduraxmon-st <2442abd72@gmail.com>
2026-03-18 17:39:32 +0500

    player added

--- src/components/maze/MazeDraw.tsx
+++ src/components/maze/MazeDraw.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { generateMaze, type Cell, type Direction } from "../../hooks/maze-build";
+import { usePlayer } from "./MazeMove";
 
 const MazeDraw = ({ row, column }: { row: number, column: number }) => {
   const [maze, setMaze] = useState<Cell[] | null>(null);
+  const player = usePlayer(maze)
   useEffect(() => {
     const stagedMaze = localStorage.getItem("maze-data")
     if (stagedMaze) {
@@ -26,6 +28,8 @@ const MazeDraw = ({ row, column }: { row: number, column: number }) => {
       className={`w-max h-max border`}>
       {
         maze?.map((block) => {
+          const isPlayer =
+            block.row === player.row && block.column === player.col;
           const hasMove = (dir: Direction) => block.canMove.includes(dir);
           const borders = `
             ${!hasMove("top") ? "border-t" : ""}
@@ -35,12 +39,15 @@ const MazeDraw = ({ row, column }: { row: number, column: number }) => {
           `;
 
           return (
-            <div className={`${borders} size-20 
+            <div className={`${borders} flex items-center justify-center size-20 
             ${block.type === "start" ? "bg-blue-500" :
                 block.type === "finish" ? "bg-green-500" : ""
               }
             `}
               key={block.key}>
+              {isPlayer && (
+                <div className="size-10 rounded-full bg-red-500" />
+              )}
             </div>
           )
         })
--- src/data/maze-data.ts
+++ src/data/maze-data.ts
@@ -12,3 +12,127 @@ export const moving = [
     type: "block"
   }
 ]
+
+
+const mazetemplate = [
+  {
+    "key": 1,
+    "row": 0,
+    "column": 0,
+    "canMove": [
+      "bottom"
+    ],
+    "type": "start",
+    "visited": true
+  },
+  {
+    "key": 2,
+    "row": 0,
+    "column": 1,
+    "canMove": [
+      "bottom",
+      "right"
+    ],
+    "type": "block",
+    "visited": true
+  },
+  {
+    "key": 3,
+    "row": 0,
+    "column": 2,
+    "canMove": [
+      "left",
+      "right"
+    ],
+    "type": "block",
+    "visited": true
+  },
+  {
+    "key": 4,
+    "row": 0,
+    "column": 3,
+    "canMove": [
+      "left",
+      "bottom"
+    ],
+    "type": "block",
+    "visited": true
+  },
+  {
+    "key": 5,
+    "row": 0,
+    "column": 4,
+    "canMove": [
+      "right"
+    ],
+    "type": "block",
+    "visited": true
+  },
+  {
+    "key": 6,
+    "row": 0,
+    "column": 5,
+    "canMove": [
+      "bottom",
+      "right",
+      "left"
+    ],
+    "type": "block",
+    "visited": true
+  },
+  {
+    "key": 7,
+    "row": 0,
+    "column": 6,
+    "canMove": [
+      "left",
+      "bottom"
+    ],
+    "type": "block",
+    "visited": true
+  },
+  {
+    "key": 8,
+    "row": 0,
+    "column": 7,
+    "canMove": [
+      "right"
+    ],
+    "type": "block",
+    "visited": true
+  },
+  {
+    "key": 9,
+    "row": 0,
+    "column": 8,
+    "canMove": [
+      "bottom",
+      "right",
+      "left"
+    ],
+    "type": "block",
+    "visited": true
+  },
+  {
+    "key": 10,
+    "row": 0,
+    "column": 9,
+    "canMove": [
+      "left",
+      "bottom"
+    ],
+    "type": "block",
+    "visited": true
+  },
+  {
+    "key": 11,
+    "row": 1,
+    "column": 0,
+    "canMove": [
+      "top",
+      "bottom"
+    ],
+    "type": "block",
+    "visited": true
+  }
+]
